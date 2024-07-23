8046e6c1b20c66f6f606d0efd200775941b65a9e
emkay <emkaycodes@gmail.com>
2024-07-23 08:02:21 +0530

    js import export statements

---
+++ test_project/src/assets/scripts/app.js
@@ -0,0 +1,21 @@
+// import { apiKey } from "./util.js";
+
+//another way to import as js objects
+
+import * as utils from "./util.js"
+
+import defaultexportvalue from "./util.js"
+
+// console.log(apiKey);
+console.log(defaultexportvalue + " default export value")
+// console.log("this is muralikrishnan");
+
+console.log(utils.name)
+
+
+
+
+
+// https://codesandbox.io/p/sandbox/first-react-app-start-forked-sk4h29?file=%2Fsrc%2FApp.js
+
+
---
+++ test_project/src/assets/scripts/util.js
@@ -0,0 +1,8 @@
+export let apiKey = "werwe32df23dfqf4dfjsdjnfkwru9234"
+
+/* another way to export using default keyword
+each file should have only one default export
+*/
+
+export default "rfjofofnonfofoofeo"
+export let name = "muralikrishnan"
