6df5148ab322a8a373d3868686ef5aadab53755a
emkay <emkaycodes@gmail.com>
2024-07-23 21:05:45 +0530

    js-refresher-23--7-2024

--- test_project/src/assets/main.js
+++ test_project/src/assets/main.js
@@ -1,2 +1,104 @@
 let uname = "muralikrishnan"
 console.log(uname);
+
+// defining a fuction
+
+function combine(a, b, c){
+    return a * b / c;
+}
+
+console.log(combine(43, 21, 5))
+
+/* defining a function using arrow notations
+    that is defining a anonymous function
+    */
+
+let a = (userName) => {console.log(userName)};
+console.log(a("tarantino"));
+
+const squareIt = x => {
+    return x*2;
+  }
+console.log(squareIt(5));
+
+const user = {
+    name: "emkay",
+    id: 12,
+    // we can even add a fuction inside the object
+    greet(){
+        console.log("hello, there!!");
+
+        console.log(this.name);
+    }
+};
+
+console.log(user);
+console.log(user.id);
+console.log(user.greet());
+
+console.log("******************************************");
+
+const films = ["got", "brba", "theboys", "cars", ]
+console.log(films[2])
+films.push("bvs")
+console.log(films);
+
+const filmIndex = films.findIndex((item) => {
+    return item === "brba";
+});
+console.log(filmIndex);
+
+const filmLang = films.map((item) => {
+    return item + " - Eng";
+});
+console.log(filmLang)
+
+function transformToObjects(numberArray) {
+    numberArray.map((item) => ({val: item}))
+}
+console.log(transformToObjects([1, 2, 3]))
+
+console.log("******************************************");
+
+// Destructuring arrays and objects
+
+const userNameData = ["murali", "krishnan", "emkay"]
+const firstName1 = userNameData[0]
+const firstName2 = userNameData[1]
+
+//now the above array can be destructed as
+
+const [firstNameA, firstNameB, firstNameC] = ["murali", "krishnan", "emkay"];
+console.log(firstNameA);
+console.log(firstNameC);
+
+// Object destructuring
+
+const userObj = {
+    name: "emkay",
+    id: 12
+}
+
+const objName = userObj.name
+
+// the above object is destructured as 
+
+const {name, id} = {
+    name: "homelander",
+    id: 47
+}
+console.log(name)
+console.log(id)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test_project/src/assets/scripts/app.js
+++ test_project/src/assets/scripts/app.js
@@ -2,6 +2,7 @@
 
 //another way to import as js objects
 
+import { func } from "prop-types"
 import * as utils from "./util.js"
 
 import defaultexportvalue from "./util.js"
@@ -12,9 +13,13 @@ console.log(defaultexportvalue + " default export value")
 
 console.log(utils.name)
 
+// defining a fuction
 
+function combine(a, b, c){
+    return a * b / combine;
+}
 
-
+console.log(combine(43, 21, 5))
 
 // https://codesandbox.io/p/sandbox/first-react-app-start-forked-sk4h29?file=%2Fsrc%2FApp.js
 
