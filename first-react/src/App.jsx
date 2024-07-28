3083c80af6f498ddd452ebf41df8dc7db0065127
emkay <emkaycodes@gmail.com>
2024-07-28 10:46:19 +0530

    folder restructure

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -1,71 +1,36 @@
-import reactImg from "./assets/react-core-concepts.png";
+import Header from "./components/Header/Header.jsx";
+import HeaderTwo from "./components/HeaderTwo.jsx";
+import CoreConcepts from "./components/CoreComponent.jsx";
+
 // import componentImg from "./assets/component-1.png";
 import { CORE_CONCEPTS } from "./data";
-
-const filler = ["Core", " Fundamental", "Crucial"];
-
-function getRandomFiller(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const fill = filler[getRandomFiller(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {fill} React concepts you will need for almost any app you are going to
-        build!
-      </p>
-    </header>
-  );
-}
-
-function HeaderTwo() {
-  return (
-    <main>
-      <h2>Time to get started!</h2>
-    </main>
-  );
-}
-
-function CoreConcepts(props) {
-  return (
-    <li>
-      <img src={props.img} alt="..." />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
     <div>
       <Header></Header>
       <HeaderTwo></HeaderTwo>
-      <section id="core-concepts"></section>
-      <h2>Core Concepts</h2>
-      <ul>
-        <CoreConcepts
-          title={CORE_CONCEPTS[0].title}
-          description={CORE_CONCEPTS[0].description}
-          img={CORE_CONCEPTS[0].image}
-        />
-        <CoreConcepts {...CORE_CONCEPTS} />
-        <CoreConcepts
-          title={CORE_CONCEPTS[2].title}
-          description={CORE_CONCEPTS[2].description}
-          img={CORE_CONCEPTS[2].image}
-        />
-        <CoreConcepts
-          title={CORE_CONCEPTS[3].title}
-          description={CORE_CONCEPTS[3].description}
-          img={CORE_CONCEPTS[3].image}
-        />
-      </ul>
+      <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcepts
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+            image={CORE_CONCEPTS[0].image}
+          />
+          <CoreConcepts
+            title={CORE_CONCEPTS[1].title}
+            description={CORE_CONCEPTS[1].description}
+            image={CORE_CONCEPTS[1].image}
+          />
+          <CoreConcepts
+            title={CORE_CONCEPTS[2].title}
+            description={CORE_CONCEPTS[2].description}
+            image={CORE_CONCEPTS[2].image}
+          />
+          <CoreConcepts {...CORE_CONCEPTS[3]} />
+        </ul>
+      </section>
     </div>
   );
 }
---
+++ first-react/src/components/HeaderTwo.jsx
@@ -0,0 +1,7 @@
+export default function HeaderTwo() {
+  return (
+    <main>
+      <h2>Time to get started!</h2>
+    </main>
+  );
+}
